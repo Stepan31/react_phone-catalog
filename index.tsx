734db77b4688d45fb52b9da1937726487ae98934
Stepan Bahrynivskyi <bagrunivskiystepan@gmail.com>
2025-04-02 22:12:20 +0300

    Updates

--- index.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { Root } from './Root';
-import './fonts.scss';
-import React from 'react';
-
-createRoot(document.getElementById('root') as HTMLDivElement).render(<Root />);
