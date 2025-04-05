6bd750a04f19a06e7606cf308dfa5edf2f14e66d
Stepan31 <136714954+Stepan31@users.noreply.github.com>
2025-04-05 15:53:27 +0000

    Deploying to gh-pages from @ Stepan31/react_phone-catalog@19b7ed9f094cb6af5890f8286812f36c87c86e6a 🚀

--- Pages/PageNotFound.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const PageNotFound: React.FC = () => (
-  <div>
-    <h1 className="title">Page not found</h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
-  </div>
-);
